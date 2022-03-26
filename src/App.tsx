7779d6de88710d96b807464660d7d36f58d6fb3f
pablohga <pablohga@gmail.com>
2022-03-26 15:58:10 +0000

    Respostas certas e erradas unificadas, random de posiçao de respostas, contador de pontos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { FormProvider } from './contexts/FormContext';
 import { Router } from './router';
 
+/* import './components/Questionaire/style.scss'; */
+
 const App = () => {
   return (
     <FormProvider>
